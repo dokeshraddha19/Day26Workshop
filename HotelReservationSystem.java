b20c302b915e99ae57ed0876e59b7166011e4ad4
dokeshraddha19 <shraddhadoke1999@gmail.com>
2021-08-23 08:50:07 +0530

    Welcome commit

---
+++ HotelReservationSystem.java
@@ -0,0 +1,10 @@
+package Hotelchain;
+
+public class HotelReservationSystem {
+	public static void main(String[] args) {
+		welcome();
+	}
+	public static void welcome() {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+}
